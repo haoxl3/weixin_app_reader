f47163e2c72567d52a53be3be9485face1f93590
haoxl3 <1098262516@qq.com>
2018-01-01 18:24:34 +0800

    创建假数据

---
+++ data/posts-data.js
@@ -0,0 +1,24 @@
+var local_database = [
+  {
+    date: "Sep 18 2016",
+    title: "正是虾蟹肥壮时",
+    imgSrc: "/images/post/crab.png",
+    content: "文本内容文本内容文本内容文本内容文本内容文本内容文本内容文本内容文本内容文本内容文本内容",
+    reading: "112",
+    collection: "96",
+    avatar: "/images/avatar/1.png"
+  },
+  {
+    date: "Sep 18 2016",
+    title: "正是虾蟹肥壮时",
+    imgSrc: "/images/post/crab.png",
+    content: "文本内容文本内容文本内容文本内容文本内容文本内容文本内容文本内容文本内容文本内容文本内容",
+    reading: "112",
+    collection: "96",
+    avatar: "/images/avatar/1.png"
+  }
+]
+
+module.exports = {
+  postList: local_database
+}
