67b539c9d878a7b3914a818005c76fd647ecb253
haoxl3 <1098262516@qq.com>
2018-01-02 23:40:21 +0800

    share

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -90,10 +90,31 @@ Page({
     var postCollected = postsCollected[this.data.currentPostId];
     postCollected = !postCollected
     postsCollected[this.data.currentPostId] = postCollected;
+    this.showToast(postsCollected, postCollected)
+  },
+  showToast: function(postsCollected, postCollected){
     wx.setStorageSync('posts_collected', postsCollected)
     //更新数据绑定变量，从而实现切换图片
     this.setData({
       collected: postCollected
+    })
+    //微信提供的组件
+    wx.showToast({
+      title: postCollected ? "收藏成功" : "取消成功"
+    })
+  },
+  //底部弹出分享
+  onShareTap: function(event){
+    var itemList = ["分享给微信好友", "分享到朋友圈", "分享到QQ", "分享到微博"]
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: "#405f80",
+      success: function(res){
+        wx.showModal({
+          title: "用户"+itemList[res.tapIndex],
+          content:"用户是否取消？"+res.cancel+"现在无法实现分享功能"
+        })
+      }
     })
   }
 })
