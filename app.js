7f199c08795828aa6fee1ca75c73aa7a8b7127c3
haoxl3 <1098262516@qq.com>
2018-01-08 23:52:13 +0800

    从豆瓣请求数据

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ App({
     //音乐是否正在被播放
     g_isPlayingMusic: false,
     //哪一个音乐正在被播放
-    g_currentMusicPostId: null
+    g_currentMusicPostId: null,
+    doubanBase: "https://api.douban.com"
   }
 })
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,3 +1,30 @@
+var app = getApp();//默认取app.js
 Page({
-  
+  onLoad: function(event){
+    var inTheatersUrl = app.globalData.doubanBase +"/v2/movie/in_theaters";
+    var comingSoonUrl = app.globalData.doubanBase +"/v2/movie/coming_soon";
+    var top250Url = app.globalData.doubanBase +"/v2/movie/top250";
+    this.getMovieListData(inTheatersUrl);
+    this.getMovieListData(comingSoonUrl);
+    this.getMovieListData(top250Url);
+  },
+  getMovieListData: function(url){
+    wx.request({
+      url: url,
+      data: {},
+      method: "GET",
+      header: {
+        "Content-Type": "application/xml"//此处为不豆瓣的bug，正常应为application/json
+      },
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (error) {
+        console.log(error)
+      },
+      complete: function () {
+
+      }
+    })
+  }
 })
